46868570663a5299acda5c4985d088415372964f
ela-hidri <elahidri.eh@gmail.com>
2023-10-04 21:45:19 +0000

    adding 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,21 @@
 #!/usr/bin/env bash
-# This script sets up your web servers for the deployment of web_static
-
-# installing nginx
+# sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 
-# creating directories if they don't already exit
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-
-# create random html file to test nginx config
-echo "<html>
+sudo bash -c "echo \"<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>\" > /data/web_static/releases/test/index.html"
 
-# check if symlink exist, if so delete it
-if [ -L "/data/web_static/current" ]; then
-	sudo rm "/data/web_static/current"
-fi
-
-# create symbolic link
-sudo ln -s "/data/web_static/releases/test/" "/data/web_static/current"
-
-# change ownership of /data/ and every folder/file inside
+rm -f /data/web_static/current
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 sudo chown -R ubuntu:ubuntu /data/
-
-# update nginx config
-sudo sed -i '/listen 80 default_server;/a\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-
-# check the syntax errors
+sudo sed -i "\t location /hbnb_static/ {\n\talias https://mydomainname.tech/hbnb_static;\n}" /etc/nginx/sites-available/default
 sudo nginx -t
-
-# restart nginx
 sudo service nginx restart

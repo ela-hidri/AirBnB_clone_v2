69e45f7e8453b82190159c6fbebe4c222db2ee2a
ela-hidri <elahidri.eh@gmail.com>
2023-10-05 19:08:02 +0000

    fixing file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,6 +16,5 @@ sudo bash -c "echo \"<html>
 rm -f /data/web_static/current
 sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/listen 80 default_service;/a\\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n}" /etc/nginx/sites-available/default
-sudo nginx -t
+sudo sed -i '/listen 80 default_server;/a\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart

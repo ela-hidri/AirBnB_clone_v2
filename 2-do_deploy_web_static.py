83e4b9268b8f2cb61830bbca149849dc10867389
ela-hidri <elahidri.eh@gmail.com>
2023-10-05 21:11:35 +0000

    fixing file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,16 +12,17 @@ def do_deploy(archive_path):
     """distribute an archive"""
     if os.path.isfile(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    tmp = "/tmp/{}".format(file)
-    ohne_tgz = "/data/web_static/releases/".format(file.split(".")[0])
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+
     try:
-        put(archive_path, "/tmp/{}".format(file))
-        run("mkdir -p {}/".format(ohne_tgz))
-        run("tar -xzf {} -C {}".format(tmp, ohne_tgz))
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
         run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(ohne_tgz, ohne_tgz))
-        run("rm -rf {}/web_static".format(ohne_tgz))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
         run("rm -rf /data/web_static/current")
         run("ln -s {}/ /data/web_static/current".format(no_tgz))
         return True
